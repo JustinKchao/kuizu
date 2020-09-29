7e14d775f4d49252b01d026bfab1e90985880bb1
Justin Kchao <Justin.Kchao@gmail.com>
2020-09-28 20:22:03 -0700

    Updates

---
+++ precache-manifest.a11a7456f077275a1035f64e53e54b29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7237d99a497df1540bd9105a2a19eb78",
+    "url": "/kuizu/index.html"
+  },
+  {
+    "revision": "7794c82c0b061c0b230e",
+    "url": "/kuizu/static/css/main.1ee23c45.chunk.css"
+  },
+  {
+    "revision": "26e265928164e5a9f782",
+    "url": "/kuizu/static/js/2.e17681eb.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/kuizu/static/js/2.e17681eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7794c82c0b061c0b230e",
+    "url": "/kuizu/static/js/main.c3339d38.chunk.js"
+  },
+  {
+    "revision": "959f984aa513e5449c07",
+    "url": "/kuizu/static/js/runtime-main.c76c4f5d.js"
+  }
+]);
